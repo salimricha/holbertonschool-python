5e3755a0c0c899027c44b715b45ec8341fa249be
Salimricha <salim.richa12@gmail.com>
2020-09-03 12:35:33 +0000

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,7 +4,7 @@ from add_0 import add
 
 def import_add(a, b):
     print('{} + {} = {}'.format(a, b, add(a, b)))
-    if _name_ == '_main_':
-        a = 1
-        b = 2
+if _name_ == '_main_':
+    a = 1
+    b = 2
 import_add(a, b)
