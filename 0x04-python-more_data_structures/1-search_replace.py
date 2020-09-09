a11af57fa8ba4eb6bd3b9b24efe7b0df397ba824
Salimricha <salim.richa12@gmail.com>
2020-09-09 13:03:50 +0000

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    for idx, item in enumerate(my_list):
+        if item == search:
+            my_list[idx] = replace
+    return my_list
