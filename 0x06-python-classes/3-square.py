37be596f8028e647f05f63f67afdd1c10d4dd73c
Salimricha <salim.richa12@gmail.com>
2020-09-17 17:35:01 +0000

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''this .....'''
+
+
+class Square:
+    ''' thiss '''
+    def __init__(self, size=0):
+        ''' thiss'''
+        self.__size = size
+
+
+def size(self, value):
+        ''' this '''
+        if value.is_integer():
+            self.__size = value
+        else:
+            raise TypeError
+
+
+def getArea(self):
+    '''this'''
+    return int(self.__size)**2
