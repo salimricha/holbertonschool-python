1a6a1c650864ce85abfd34934e024fb561913478
Salimricha <salim.richa12@gmail.com>
2020-09-17 18:11:08 +0000

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
 '''this .....'''
+
+
 class Square:
     ''' thiss '''
-    def __init__(self,size=0):
+    def __init__(self, size=0):
         ''' thiss'''
         self.__size = size
-    def size(self,value):
+
+
+def size(self, value):
         ''' this '''
         if value.is_integer():
             self.__size = value
         else:
             raise TypeError
-    def getArea(self):
-        '''this'''
-        return int(self.__size)**2
+
+
+def getArea(self):
+    '''this'''
+    return int(self.__size)**2
+
+
 def main():
+    '''this'''
     aSquare = Square()
-    
